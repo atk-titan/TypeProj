f2dda1de4628d8d36e60ad963599471a51f59731
atk-titan <gauravmane8451@gmail.com>
2025-11-20 23:57:32 +0530

    prisma setup done

---
+++ app/api/single-player/routes.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(req:NextRequest){
+
+    return NextResponse.json({
+        
+    },{
+        status:200
+    })
+}
